2894f29c75b0b0baed85293828200a448b77c604
Dial2025 <ouidir@et.esiea.fr>
2021-12-07 14:24:48 +0100

    EndPointsHTTP

---
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -0,0 +1,24 @@
+package fr.lernejo.todo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoListController {
+
+    private final List<Todo> todoList = new ArrayList<Todo>();
+
+    @RequestMapping(value = ("/api/todo"), method=RequestMethod.POST, consumes = {"application/json"})
+    public void addTodo(@RequestBody Todo todo) {
+        todoList.add(todo);
+    }
+
+    @RequestMapping(value = ("/api/todo"),  method=RequestMethod.GET)
+    public List<Todo> viewTodo() {
+        return todoList;
+    }
+}
+
+
